5e8a1f2ca60467ab1a31d2fd1ce621338bb46ed1
Roey Darwish Dror <rodarwis@microsoft.com>
2021-09-10 15:58:43 +0300

    Formatting

--- src/main/java/xyz/darwish/seleniumbootstarter/WebDriverFactory.java
+++ src/main/java/xyz/darwish/seleniumbootstarter/WebDriverFactory.java
@@ -1,8 +1,6 @@
 package xyz.darwish.seleniumbootstarter;
 
 import org.openqa.selenium.WebDriver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 public interface WebDriverFactory {
     WebDriver webDriver();
